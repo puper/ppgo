81394e57c95292209a2cc63cdbaea06840ae8422
Pu Pu <pu.pu@mydreamplus.com>
2018-11-29 16:27:18 +0800

    update

--- components/dbman/transaction.go
+++ components/dbman/transaction.go
@@ -2,13 +2,13 @@ package dbman
 
 import "github.com/jinzhu/gorm"
 
-func (DBMan) Transaction(db *gorm.DB, f func() error) error {
+func (DBMan) Transaction(db *gorm.DB, f func(tx *gorm.DB) error) error {
 	tx := db.Begin()
-	err := f()
+	err := f(tx)
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
 	tx.Commit()
-	return nil
+	return tx.Error
 }
