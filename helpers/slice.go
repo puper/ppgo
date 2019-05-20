3c0bfe712d5978c027d25938b4a11c3df7fdffff
Pu Pu <pu.pu@mydreamplus.com>
2019-05-20 15:47:51 +0800

    add sliceUnique

--- helpers/slice.go
+++ helpers/slice.go
@@ -16,3 +16,32 @@ func SlicePluck(key string, in, out interface{}) error {
 	}
 	return nil
 }
+
+func SliceUnique(s interface{}, uf func(i int) interface{}) {
+	rv := reflect.ValueOf(s)
+	if rv.Kind() == reflect.Interface || rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
+	}
+	if rv.Kind() != reflect.Slice {
+		panic("s not a slice")
+	}
+	uniqueMap := map[interface{}]bool{}
+	j := 0
+	for i := 0; i < rv.Len(); i++ {
+		rv1 := rv.Index(i)
+		var k interface{}
+		if uf == nil {
+			k = rv1.Interface()
+		} else {
+			k = uf(i)
+		}
+		if uniqueMap[k] {
+			continue
+		} else {
+			uniqueMap[k] = true
+			rv.Index(j).Set(rv1)
+			j++
+		}
+	}
+	rv.SetLen(j)
+}
